204c0d50ae8d362219ef26e226e2afe16acddba3
洋芋片 <yuchengyang9487@gmail.com>
2024-12-05 21:31:50 +0800

    2024/12/5更新檔
    1.更新遊戲大廳栽培園和公會。

---
+++ Assets/MyAssets/UI/Script/UIPlantationGarden.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIPlantationGarden : UIBase
+{
+    [Header("工具")]
+    [SerializeField]
+    private Button fertilizeButton;
+    [SerializeField]
+    private Button harvestButton;
+    [SerializeField]
+    private Button wateringButton;
+    [SerializeField]
+    private Button shopButton;
+
+}
--- Assets/MyAssets/UI/Script/UISkyIsland.cs
+++ Assets/MyAssets/UI/Script/UISkyIsland.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UISkyIsland : UIBase
 {
-
+    [SerializeField]
+    private Image plantationGardenMenu;
+    [SerializeField]
+    private Image guildMenu;
+    [SerializeField]
+    private Image shopMenu;
 }
